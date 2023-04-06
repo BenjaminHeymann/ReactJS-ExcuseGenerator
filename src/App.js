a460388984a969f3ce36d79a58a6f327b1e65abd
Benjamin <github@bretler.ch>
2023-04-06 12:56:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  let [excuse, setExcuse] = useState(null);
+
+  const getExcuseParty = () => {
+    Axios.get("https://excuser-three.vercel.app/v1/excuse/party/").then(
+      (res) => {
+        setExcuse(res.data[0]);
+      }
+    );
+  };
+
+  const getExcuseFamily = () => {
+    Axios.get("https://excuser-three.vercel.app/v1/excuse/family/").then(
+      (res) => {
+        setExcuse(res.data[0]);
+      }
+    );
+  };
+
+  const getExcuseOffice = () => {
+    Axios.get("https://excuser-three.vercel.app/v1/excuse/office/").then(
+      (res) => {
+        setExcuse(res.data[0]);
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Generate an Excuse</h1>
+      <button onClick={getExcuseParty}>Party</button>
+      <button onClick={getExcuseFamily}>Family</button>
+      <button onClick={getExcuseOffice}>Office</button>
+      <h2>{excuse.excuse}</h2>
+      <h3>{excuse.category}</h3>
     </div>
   );
 }
